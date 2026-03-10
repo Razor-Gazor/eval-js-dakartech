b556ed9f9e543a9d4ac4208c371e5d2a303394cd
Razor Gazor <sololilon@gmail.com>
2026-03-10 15:41:45 +0000

    affichage taches dans DOM et maj du compteur

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const zoneErreur= document.getElementById('zone-erreur');
 formulaire.addEventListener('submit', function(event){
     event.preventDefault();
     
-    const texteTache = entreeUtilisateur.value.trim();
+    const texteTache= entreeUtilisateur.value.trim();
 
     if (texteTache === ""){
         zoneErreur.textContent = "Veuillez saisir une tâche.";
@@ -19,10 +19,39 @@ formulaire.addEventListener('submit', function(event){
         texte: texteTache,
         terminee: false
     };
-
+    
     taches.push(nouvelleTache);
+    afficherTaches();
 
     entreeUtilisateur.value= "";
 
     console.log(taches);
 });
+
+function afficherTaches(){
+    const liste = document.getElementById('liste-taches');
+    const compteur = document.getElementById('compteur-taches');
+    
+    liste.innerHTML = "";
+    
+    let nbEnCours = 0;
+
+    taches.forEach(function(tache, index) {
+        const li = document.createElement('li');
+        li.className = "tache-item";
+        
+        if (tache.terminee){
+            li.classList.add('tache-terminee');
+        } 
+        
+        else {
+            nbEnCours++; 
+        }
+
+        li.innerHTML = tache.texte + 
+            ' <button>Terminé</button>' + 
+            ' <button>Supprimer</button>';
+        liste.appendChild(li);
+    });
+    compteur.textContent = nbEnCours;
+}
